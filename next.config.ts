bdf1fe394cea8e7d11ad678fefcb171f2eb11141
Xantass <theo.fouque@epitech.eu>
2025-01-31 03:30:51 -0500

    fix: config minimum for the deployement

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  reactStrictMode: false,
+  images: {
+    unoptimized: false,
+  },
+  assetPrefix: '',
+  basePath: '',
+  output: 'export'
 };
 
 export default nextConfig;
