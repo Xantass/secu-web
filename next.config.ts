f139f81f09fdfed4dab36729fedf0d2b1661d06d
Xantass <theo.fouque@epitech.eu>
2025-01-31 02:50:40 -0500

    fix: config deploy use different .env for the deploy and local && and use absolute path for the img src

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-  assetPrefix: '/secu-web',
-  basePath: '/secu-web',
+  assetPrefix: isProd ? '/secu-web' : '',
+  basePath: isProd ? '/secu-web' : '',
   output: 'export'
 };
 
