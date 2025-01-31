3dc501e03f7f24ecfa2bf593c8d56adc8d18e5bd
Xantass <theo.fouque@epitech.eu>
2025-01-31 03:08:35 -0500

    fix: delete all config for deploy github pages

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: isProd ? '/secu-web' : '',
-  basePath: isProd ? '/secu-web' : '',
-  output: 'export'
+
 };
 
 export default nextConfig;
